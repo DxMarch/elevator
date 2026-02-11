ee2f697b34cfdda2a355bf65b5248904aa9054b8
Magnus <magnuseifr@gmail.com>
2026-02-11 13:11:31 +0100

    feat: setup hallorders module interface and initialization

--- lib/elevator/hall_orders.ex
+++ lib/elevator/hall_orders.ex
@@ -2,5 +2,36 @@ defmodule Elevator.HallOrders do
   @moduledoc """
   Module responsible for all changes occuring to the hall_order part of the state.
   """
-  # TODO: Implement
+  use GenServer
+
+  @type state_t :: Elevator.State.hall_order_map()
+
+  def start_link(arg) do 
+    GenServer.start_link(__MODULE__, arg, name: __MODULE__)
+  end
+
+  @spec init(any()) :: {:ok, state_t()}
+  def init(num_floors) do
+    state = Range.new(0, num_floors - 1)
+    |> Enum.flat_map(fn floor -> [{floor, :up}, {floor, :down}] end)
+    |> Enum.map(&{&1, :unknown})
+    |> Enum.into(%{})
+    {:ok, state}
+  end
+
+  @spec receive_state(state_t()) :: :ok
+  def receive_state(other_state), do: GenServer.cast(__MODULE__, {:receive_state, other_state})
+
+  @spec button_press(non_neg_integer(), :up | :down) :: :ok
+  def button_press(floor, button_type), do: GenServer.cast(__MODULE__, {:button_press, floor, button_type})
+
+  @spec handle_cast({:receive_state, state_t()}, state_t()) :: {:noreply, state_t()}
+  def handle_cast({:receive_state, _other_state}, state) do
+    {:noreply, state}
+  end
+
+  @spec handle_cast({:button_press, non_neg_integer(), :up | :down}, state_t()) :: {:noreply, state_t()}
+  def handle_cast({:button_press, _floor, _direction}, state) do
+    {:noreply, state}
+  end
 end
--- test/elevator_test.exs
+++ test/elevator_test.exs
@@ -2,7 +2,13 @@ defmodule ElevatorTest do
   use ExUnit.Case
   doctest Elevator
 
-  test "greets the world" do
-    assert Elevator.hello() == :world
+  @tag :hall_orders
+  test "initializes state with unknown values" do
+    {:ok, state} = Elevator.HallOrders.init(2)
+    assert map_size(state) == 4
+    assert state[{0, :down}] == :unknown
+    assert state[{0, :up}] == :unknown
+    assert state[{1, :down}] == :unknown
+    assert state[{1, :up}] == :unknown
   end
 end
