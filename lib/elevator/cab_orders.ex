dad08c8c1696a4d20a4b6047e78d918ca29a6aaf
Magnus <magnuseifr@gmail.com>
2026-02-18 11:13:39 +0100

    fix: proper entry point

--- lib/elevator/cab_orders.ex
+++ lib/elevator/cab_orders.ex
@@ -1,10 +1,22 @@
 defmodule Elevator.CabOrders do
+  use GenServer
   @moduledoc """
   Module responsible for all changes occuring to the cab_order part of the state.
   """
-  # TODO: Implement
+
+  def init(arg) do
+    {:ok, arg}
+  end
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
 
   def get_orders do
-    []
+    GenServer.call(__MODULE__, :get_orders)
+  end
+
+  def handle_call(:get_orders, _, state) do
+    {:reply, MapSet.new([]), state}
   end
 end
