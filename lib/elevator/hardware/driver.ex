a173668d0ebd6a2c55a9be5a58acd971954810b4
Magnus <magnuseifr@gmail.com>
2026-03-19 16:18:26 +0100

    chore: cleanup driver

--- lib/elevator/hardware/driver.ex
+++ lib/elevator/hardware/driver.ex
@@ -1,16 +1,20 @@
 defmodule Elevator.Hardware.Driver do
+  @moduledoc """
+  Handout driver from TTK4145 GitHub with minor modifications.
+
+  Modifications:
+  - Don't crash immediately if tcp connection fails. Retry connection periodically instead.
+  - Added @spec declarations to public functions.
+  - Changed magic numbers to atoms where applicable.
+  """
   use GenServer
   require Logger
 
   @call_timeout_ms 1000
   @reconnect_interval_ms 1000
-  @button_map %{:hall_up => 0, :hall_down => 1, :cab => 2}
-  @state_map %{:on => 1, :off => 0}
-  @direction_map %{:up => 1, :down => 255, :stop => 0}
-
-  def start_link([]) do
-    start_link([{127, 0, 0, 1}, 15657])
-  end
+  @button_map %{hall_up: 0, hall_down: 1, cab: 2}
+  @state_map %{on: 1, off: 0}
+  @direction_map %{up: 1, down: 255, stop: 0}
 
   def start_link([address, port]) do
     GenServer.start_link(__MODULE__, [address, port], name: __MODULE__)
@@ -43,119 +47,50 @@ defmodule Elevator.Hardware.Driver do
   end
 
   # User API ----------------------------------------------
-  @doc """
-  Sets the motor direction.
-
-  ## Parameters
-  - direction: :up, :down, or :stop
-  """
+  @spec set_motor_direction(:up | :down | :stop) :: :ok
   def set_motor_direction(direction) do
     GenServer.cast(__MODULE__, {:set_motor_direction, direction})
   end
 
-  @doc """
-  Sets the light for an order button.
-
-  ## Parameters
-  - button_type: :hall_up, :hall_down, or :cab
-  - floor: floor number (integer)
-  - state: :on or :off
-  """
+  @spec set_order_button_light(:hall_up | :hall_down | :cab, non_neg_integer(), :on | :off) :: :ok
   def set_order_button_light(button_type, floor, state) do
     GenServer.cast(__MODULE__, {:set_order_button_light, button_type, floor, state})
   end
 
-  @doc """
-  Sets the floor indicator display.
-
-  ## Parameters
-  - floor: floor number to display (integer)
-  """
+  @spec set_floor_indicator(non_neg_integer()) :: :ok
   def set_floor_indicator(floor) do
     GenServer.cast(__MODULE__, {:set_floor_indicator, floor})
   end
 
-  @doc """
-  Sets the stop button light.
-
-  ## Parameters
-  - state: :on or :off
-  """
+  @spec set_stop_button_light(:on | :off) :: :ok
   def set_stop_button_light(state) do
     GenServer.cast(__MODULE__, {:set_stop_button_light, state})
   end
 
-  @doc """
-  Sets the door open indicator light.
-
-  ## Parameters
-  - state: :on or :off
-  """
+  @spec set_door_open_light(:on | :off) :: :ok
   def set_door_open_light(state) do
     GenServer.cast(__MODULE__, {:set_door_open_light, state})
   end
 
-  @doc """
-  Gets the state of an order button.
-
-  ## Parameters
-  - floor: floor number (integer)
-  - button_type: :hall_up, :hall_down, or :cab
-
-  ## Returns
-  - :active if button is pressed
-  - :inactive if button is not pressed
-  """
+  @spec get_order_button_state(non_neg_integer(), :hall_up | :hall_down | :cab) ::
+          :active | :inactive
   def get_order_button_state(floor, button_type) do
     GenServer.call(__MODULE__, {:get_order_button_state, floor, button_type})
   end
 
-  @doc """
-  Gets the current floor from the floor sensor.
-
-  ## Returns
-  - floor number (integer) if elevator is at a floor
-  - :between_floors if elevator is between floors
-  """
+  @spec get_floor_sensor_state() :: non_neg_integer() | :between_floors
   def get_floor_sensor_state do
     GenServer.call(__MODULE__, :get_floor_sensor_state)
   end
 
-  @doc """
-  Gets the state of the stop button.
-
-  ## Returns
-  - :active if stop button is pressed
-  - :inactive if stop button is not pressed
-  """
+  @spec get_stop_button_state() :: :active | :inactive
   def get_stop_button_state do
     GenServer.call(__MODULE__, :get_stop_button_state)
   end
 
-  @doc """
-  Gets the state of the obstruction switch (door sensor).
-
-  ## Returns
-  - :active if obstruction is detected
-  - :inactive if no obstruction
-  """
+  @spec get_obstruction_switch_state() :: :active | :inactive
   def get_obstruction_switch_state do
     GenServer.call(__MODULE__, :get_obstruction_switch_state)
-  end
-
-  @doc """
-  Checks if the driver GenServer is running.
-
-  Note: This only checks if the GenServer process is alive, not if the TCP
-  connection is active. The driver will crash and restart automatically
-  if the connection fails during operation.
-
-  ## Returns
-  - :ok if the GenServer is running
-  - {:error, :not_connected} if the state is not a valid socket (should rarely occur)
-  """
-  def ping do
-    GenServer.call(__MODULE__, :ping)
   end
 
   # Casts  ----------------------------------------------
@@ -197,7 +132,6 @@ defmodule Elevator.Hardware.Driver do
     case :gen_tcp.recv(socket, 4, @call_timeout_ms) do
       {:ok, [6, 0, 0, 0]} -> {:reply, :inactive, {socket, addr, port}}
       {:ok, [6, 1, 0, 0]} -> {:reply, :active, {socket, addr, port}}
-      {:error, reason} -> {:stop, reason, {:error, reason}, {socket, addr, port}}
     end
   end
 
@@ -208,7 +142,6 @@ defmodule Elevator.Hardware.Driver do
     case :gen_tcp.recv(socket, 4, @call_timeout_ms) do
       {:ok, [7, 0, _, 0]} -> {:reply, :between_floors, {socket, addr, port}}
       {:ok, [7, 1, floor, 0]} -> {:reply, floor, {socket, addr, port}}
-      {:error, reason} -> {:stop, reason, {:error, reason}, {socket, addr, port}}
     end
   end
 
@@ -219,7 +152,6 @@ defmodule Elevator.Hardware.Driver do
     case :gen_tcp.recv(socket, 4, @call_timeout_ms) do
       {:ok, [8, 0, 0, 0]} -> {:reply, :inactive, {socket, addr, port}}
       {:ok, [8, 1, 0, 0]} -> {:reply, :active, {socket, addr, port}}
-      {:error, reason} -> {:stop, reason, {:error, reason}, {socket, addr, port}}
     end
   end
 
@@ -230,17 +162,6 @@ defmodule Elevator.Hardware.Driver do
     case :gen_tcp.recv(socket, 4, @call_timeout_ms) do
       {:ok, [9, 0, 0, 0]} -> {:reply, :inactive, {socket, addr, port}}
       {:ok, [9, 1, 0, 0]} -> {:reply, :active, {socket, addr, port}}
-      {:error, reason} -> {:stop, reason, {:error, reason}, {socket, addr, port}}
     end
-  end
-
-  @impl true
-  def handle_call(:ping, _from, {socket, addr, port}) when is_port(socket) do
-    {:reply, :ok, {socket, addr, port}}
-  end
-
-  @impl true
-  def handle_call(:ping, _from, state) do
-    {:reply, {:error, :not_connected}, state}
   end
 end
